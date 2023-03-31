749149d4d181da3302a53a894283ad8a0e3644c8
Rachael <124227903+Lakaeli@users.noreply.github.com>
2023-03-30 22:33:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# **Timed JavaScript Quiz Challenge**
+
+
+
+
+
+
+## Purpose
+This project was built for a class assignment. The goal was to build a timed multiple-choice question quiz.
+
+## Resources
+MDN Web Docs
+
+## Screenshots
+![ALT](/ScreenShotTimedJavaScriptQuiz.png)
---
+++ multipleChoiceQuestions.js
@@ -0,0 +1,55 @@
+// Create an Array of questions
+let questions = [
+    
+    {
+    numb: 1,
+    question: "What is a variable?",
+    answer:"Container that sores a value",
+    options:[ 
+    "Name of a container",
+    "Value of a container",
+    "Text in a container",
+    "Container that sores a value",
+    ]
+},
+{
+    numb: 2,
+    question: "for (var i = 0; i < passwordLength; i++) is an example of what?",
+    answer:"Itertaion",
+    options:[
+    "Function",
+    "variable",
+    "Itertaion",
+        "HTML code",
+]
+},{
+    numb: 3,
+    question: "True or False: A Boolean attribute, represents the true value on an element.",
+    answer: "True",    
+    options:[
+    "True",
+    "False",
+]
+
+},{
+    numb: 4,
+    question: "What does REPL stand for?",
+    answer: "Read.Execute.Print.Loop.",
+    options:[
+    "Read.Execute.Print.Loops.",
+    "Remainder.Exception.Produce.Loop.",
+    "Read.Execute.Print.Lead.",
+    "Read.Execute.Print.Loop.",
+    ]
+},{
+    numb: 5,
+    question: "What is a set of steps we use to solve a problem called?",
+    answer:"Algorithims",
+    options:[
+        "For Loops",
+        "Algorithims",
+        "Arrays",
+        "Variable names",
+    ]
+},
+];
